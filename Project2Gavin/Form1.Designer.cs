f0ac0cf439a88fecbcea7daa6ceae38be4f73a9e
verzah <100164366+verzah@users.noreply.github.com>
2023-09-29 01:19:19 -0400

    Add files via upload

---
+++ Project2Gavin/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace Project2Gavin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.lblBox = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.Location = new System.Drawing.Point(144, 19);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(242, 39);
+            this.lblHeader.TabIndex = 0;
+            this.lblHeader.Text = "Prime Numbers";
+            // 
+            // lblBox
+            // 
+            this.lblBox.AutoSize = true;
+            this.lblBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBox.Location = new System.Drawing.Point(48, 112);
+            this.lblBox.Name = "lblBox";
+            this.lblBox.Size = new System.Drawing.Size(166, 25);
+            this.lblBox.TabIndex = 1;
+            this.lblBox.Text = "Enter a # 1-100:";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(232, 112);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(76, 20);
+            this.txtInput.TabIndex = 2;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOutput.Location = new System.Drawing.Point(194, 225);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(161, 134);
+            this.lblOutput.TabIndex = 3;
+            this.lblOutput.Text = "Prime #s Displayed Here";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(211, 151);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(120, 53);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate Primes";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 368);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblBox);
+            this.Controls.Add(this.lblHeader);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Label lblBox;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ Project2Gavin/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Import System.IO in order to utilize StreamWriter to write the array to a file.
+using System.IO;
+namespace Project2Gavin
+{
+    public partial class Form1 : Form
+    {
+        //Initate variables
+        //Creates a variable bool for two outcomes, identifies if the input is prime (true, false). 
+        bool isPrime;
+        //Creates a variable int, stores the count of prime numbers from the input to input + 100.
+        int countPrimes;
+        //Creates a variable array, prime numbers are transferred and stored in.
+        int[] primesArray;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Creates method that determines if a number is prime (true, false).
+        private bool primeMethod(int input)
+        {
+            if (input == 0)
+            {
+                return false;
+            }
+            for (int i = 2; i < input; i++)
+            {
+                if (input % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        //Code for the click event on the Calculate Primes button.
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //Clear output label.
+            lblOutput.Text = "";
+            //Display error if input textbox is empty.
+            if (txtInput.Text == "")
+            {
+                MessageBox.Show("Please enter a number between 1 and 100!");
+                return;
+            }
+            //Convert input to integer to calculate primes.
+            int input = int.Parse(txtInput.Text);
+            //Display error if input is not between 1 and 100.
+            if (input < 1 || input > 100)
+            {
+                MessageBox.Show("Please enter a number between 1 and 100!");
+                return;
+            }
+            //Increase input by 100 to define the range.
+            int endRange = input + 100;
+            //Creates variable to identify index of the array for output.
+            int arrayIndex = 0;
+            //Initializes array containing 100 zeroes.
+            primesArray = new int[1000];
+            //Creates for loop to identify prime numbers within range.
+            for (int i = input; i < endRange; i++)
+            {
+                isPrime = primeMethod(i);
+                if (isPrime == true)
+                {
+                    primesArray[arrayIndex] = i;
+                    arrayIndex++;
+                    countPrimes++;
+                }
+            }
+            //Writes output to .txt.
+            StreamWriter toFile = new StreamWriter("PrimeNumbers.txt");
+            //Creates for loop to write array to .txt.
+            for (int i = 0; i < primesArray.Length - 1; i ++)
+            {
+                toFile.WriteLine(primesArray[i]);
+            }
+            toFile.Close();
+            //Displays output message in label
+            lblOutput.Text = "There are " + countPrimes + " prime numbers between " + input + " and " + endRange + ". The first is " + primesArray[0] + " and the last is " + primesArray[countPrimes - 1] + ".";
+        }
+    }
+}
